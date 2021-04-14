9a6260ae9d12fef114650ca225f678c4e9e7069e
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-04-14 20:59:11 +0900

    async method of writing and reading

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const fs = require("fs");
 
 // blocking method, synchronous
-const textIn = fs.readFileSync("./text/input.txt", "utf8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./text/input.txt", "utf8");
+// console.log(textIn);
 
-const textOut = `This is just a random string: ${textIn}\n Created on ${Date.now()}`;
+// const textOut = `This is just a random string: ${textIn}\n Created on ${Date.now()}`;
 
-fs.writeFileSync("./text/output.txt", textOut);
-console.log("File written!");
+// fs.writeFileSync("./text/output.txt", textOut);
+// console.log("File written!");
 
 // non-blocking, asychronous
+fs.readFile("./text/start.txt", "utf8", (err, data1) => {
+  fs.readFile(`./text/${data1}.txt`, "utf8", (err, data2) => {
+    fs.readFile(`./text/append.txt`, "utf8", (err, data3) => {
+      fs.writeFile(
+        "./text/final.txt",
+        `${data2} \n ${data3}`,
+        "utf8",
+        (err) => {
+          console.log("Your file has been written~");
+        }
+      );
+    });
+  });
+});
+console.log("Will read file!");
